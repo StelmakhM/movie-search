3efef9217d0f928c2e9cb24af433810c57c0d95c
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-29 20:25:00 +0200

    hidden for wrongSearch message

--- src/js/films.js
+++ src/js/films.js
@@ -2,8 +2,9 @@ import { MovieApi } from './movieApi';
 import Notiflix from 'notiflix';
 
 const refs = {
-    filmsContainer: document.querySelector('.films__list'),
-    form: document.querySelector('#header-form'),
+  filmsContainer: document.querySelector('.films__list'),
+  form: document.querySelector('#header-form'),
+  wrongSesarchEl: document.querySelector('.header__form-message'),
 };
 
 const movieApi = new MovieApi();
@@ -12,65 +13,81 @@ window.addEventListener('load', onWindowLoad);
 refs.form.addEventListener('submit', onFormSubmit);
 
 async function onWindowLoad() {
-    try {
-        const {data: {results, page, total_pages, total_results}} = await movieApi.fetchTrendingMovies();
-        const { data: { genres } } = await movieApi.fetchMoviesGenres();
-        createGenreFromId(results, genres);
-        createMarkUp(results);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const {
+      data: { results, page, total_pages, total_results },
+    } = await movieApi.fetchTrendingMovies();
+    const {
+      data: { genres },
+    } = await movieApi.fetchMoviesGenres();
+    createGenreFromId(results, genres);
+    createMarkUp(results);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function onFormSubmit(e) {
-    try {
-        e.preventDefault();
-        movieApi.query = e.currentTarget.elements.movie_title.value;
-        if(!movieApi.query) {
-            Notiflix.Notify.info(`Please, enter search query`);
-            return;
-        }
-        const {data: {results, page, total_pages, total_results}} = await movieApi.fetchQueryMovies();
-        if(!total_pages) {
-            Notiflix.Notify.failure(
-                'Sorry, there are no movies matching your search query. Please try again.'
-              );
-              return;
-        }
-        const { data: { genres } } = await movieApi.fetchMoviesGenres();
-        createGenreFromId(results, genres);
-        createMarkUp(results);
-        e.target.reset();
-    } catch (error) {
-        console.log(error);
+  try {
+    e.preventDefault();
+    movieApi.query = e.currentTarget.elements.movie_title.value;
+    if (!movieApi.query) {
+      Notiflix.Notify.info(`Please, enter search query`);
+      return;
     }
-
+    refs.wrongSesarchEl.classList.add('hidden');
+    const {
+      data: { results, page, total_pages, total_results },
+    } = await movieApi.fetchQueryMovies();
+    if (!total_pages) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no movies matching your search query. Please try again.'
+      );
+      refs.wrongSesarchEl.classList.remove('hidden');
+      return;
+    }
+    const {
+      data: { genres },
+    } = await movieApi.fetchMoviesGenres();
+    createGenreFromId(results, genres);
+    createMarkUp(results);
+    e.target.reset();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function createMarkUp(filmsArray) {
-    const  markUp = filmsArray.map(({ title, release_date, poster_path, genres }) => {
-        const releaseDate = new Date(release_date).getFullYear();
-        return ` <li class='films__item'>
+  const markUp = filmsArray
+    .map(({ title, release_date, poster_path, genres }) => {
+      const releaseDate = new Date(release_date).getFullYear();
+      return ` <li class='films__item'>
                     <a class='films__link'>
-                    <img class='films__poster' src='${movieApi.imgUrl}${movieApi.imgSize}${poster_path}' alt='${title} poster' />
+                    <img class='films__poster' src='${movieApi.imgUrl}${
+        movieApi.imgSize
+      }${poster_path}' alt='${title} poster' />
                     <div class='films__info'>
                     <h2 class='films__title'>${title}</h2>
-                    <p class='films__genres'>${(genres.length > 3) ? genres[0] + ", " + genres[1]  + ' and others' : genres.join(', ')} | ${releaseDate}</p>
+                    <p class='films__genres'>${
+                      genres.length > 3
+                        ? genres[0] + ', ' + genres[1] + ' and others'
+                        : genres.join(', ')
+                    } | ${releaseDate}</p>
                     </div></a>
                 </li>`;
-    }).join('');
+    })
+    .join('');
 
-    refs.filmsContainer.innerHTML = markUp;
+  refs.filmsContainer.innerHTML = markUp;
 }
 
-function createGenreFromId (moviesList, genreIds) {
-    moviesList.forEach(film => {
-        film.genres = [];
-        genreIds.forEach(genre => {
-            if (film.genre_ids.includes(genre.id)) {
-                film.genres.push(genre.name);
-            }
-        });
+function createGenreFromId(moviesList, genreIds) {
+  moviesList.forEach(film => {
+    film.genres = [];
+    genreIds.forEach(genre => {
+      if (film.genre_ids.includes(genre.id)) {
+        film.genres.push(genre.name);
+      }
     });
+  });
 }
-
