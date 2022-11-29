0013b848515ee12b4f63bbcf57478b0f5e6a966e
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-29 15:34:10 +0200

    search movie on form submit

--- src/js/films.js
+++ src/js/films.js
@@ -2,35 +2,42 @@ import { MovieApi } from './movieApi';
 
 const refs = {
     filmsContainer: document.querySelector('.films__list'),
+    form: document.querySelector('#header-form'),
 };
 
 const movieApi = new MovieApi();
 
 window.addEventListener('load', onWindowLoad);
+refs.form.addEventListener('submit', onFormSubmit);
 
 async function onWindowLoad() {
     try {
-        const { data: { results: trendFilms } } = await movieApi.fetchTrendingMovies();
+        const {data: {results, page, total_pages, total_results}} = await movieApi.fetchTrendingMovies();
         const { data: { genres } } = await movieApi.fetchMoviesGenres();
-        const queryFilms = await movieApi.fetchQueryMovies();
-        console.log('queryFilms :>> ', queryFilms);
-        trendFilms.forEach(film => {
-            film.genres = [];
-            genres.forEach(genre => {
-                if (film.genre_ids.includes(genre.id)) {
-                    film.genres.push(genre.name);
-                }
-            });
-        });
-        refs.filmsContainer.innerHTML = createMarkUp(trendFilms);
+        createGenreFromId(results, genres);
+        createMarkUp(results);
     } catch (error) {
         console.log(error);
     }
 }
 
+async function onFormSubmit(e) {
+    try {
+        e.preventDefault();
+        movieApi.query = e.currentTarget.elements.movie_title.value;
+        const {data: {results, page, total_pages, total_results}} = await movieApi.fetchQueryMovies();
+        const { data: { genres } } = await movieApi.fetchMoviesGenres();
+        createGenreFromId(results, genres);
+        createMarkUp(results);
+        e.target.reset();
+    } catch (error) {
+        console.log(error);
+    }
 
-function createMarkUp(array) {
-    return markUp = array.map(({ title, release_date, poster_path, genres }) => {
+}
+
+function createMarkUp(filmsArray) {
+    const  markUp = filmsArray.map(({ title, release_date, poster_path, genres }) => {
         const releaseDate = new Date(release_date).getFullYear();
         return ` <li class='films__item'>
                     <a class='films__link'>
@@ -41,4 +48,17 @@ function createMarkUp(array) {
                     </div></a>
                 </li>`;
     }).join('');
+
+    refs.filmsContainer.innerHTML = markUp;
+}
+
+function createGenreFromId (moviesList, genreIds) {
+    moviesList.forEach(film => {
+        film.genres = [];
+        genreIds.forEach(genre => {
+            if (film.genre_ids.includes(genre.id)) {
+                film.genres.push(genre.name);
+            }
+        });
+    });
 }
