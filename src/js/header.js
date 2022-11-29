f1a5f8e729da08a3fdd320976472f1c75892d723
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-29 23:00:05 +0200

    corrected import

--- src/js/header.js
+++ src/js/header.js
@@ -10,22 +10,4 @@ window.onresize = function () {
   }
 };
 
-// ----------------Переключение между кнопками-----------------
-
-const headerWatchedButtonEl = document.querySelector('.js-watched');
-const headerQueueButtonEl = document.querySelector('.js-queue');
-
-headerWatchedButtonEl.addEventListener('click', onWatched);
-headerQueueButtonEl.addEventListener('click', onQueue);
-
-function onWatched() {
-  headerWatchedButtonEl.classList.toggle('header__button--active');
-  headerQueueButtonEl.classList.remove('header__button--active');
-}
-
-function onQueue() {
-  headerWatchedButtonEl.classList.remove('header__button--active');
-  headerQueueButtonEl.classList.toggle('header__button--active');
-}
-
 // ----------------          -----------------
--- src/library.js
+++ src/library.js
@@ -1,5 +1,6 @@
 import './js/footer';
 import './js/header';
+import './js/header-library';
 import './js/modal-film';
 import './js/modal-team';
 import './js/library';
