238f6931e33ac9d964ba5452992c49ca49763476
Maryna-smile <maryna.naidych@gmail.com>
2022-11-29 21:10:44 +0200

    97

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,20 +1,37 @@
 
-(() => {
+
   const refs = {
-    openModalBtn: document.querySelector('.btn-open'),
+    // openModalBtn: document.querySelector('.btn-open'),
     closeModalBtn: document.querySelector('.modal__btn-close'),
     modal: document.querySelector('.modal'),
     body: document.body,
+    filmCard: document.querySelector('.films__list'),
+    backdropEl: document.querySelector('.backdrop')
       };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', () => {
-     toggleModal();
-  });
+  refs.filmCard.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('overflow');
+  refs.closeModalBtn.addEventListener('click', closeleModal)
+
+  function toggleModal(e) {
+    const item = e.target.closest('.films__list')
+    if (!item) {
+      return;
+    }
+    refs.backdropEl.classList.remove('is-hidden');
+    document.addEventListener('keydown', onEscDown);
+  };
+
+  function closeleModal(e) {
+    refs.backdropEl.classList.add('is-hidden');
+    document.removeEventListener('keydown', onEscDown);
   }
-})();
 
+document.addEventListener('keydown', onEscDown);
+function onEscDown(e) {
+if (e.code === "Escape") {
+  closeleModal();
+  console.log(e.code)
+}
+
+}
