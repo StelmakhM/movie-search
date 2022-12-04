f925dc7636f316df3f170e1ab737bce02a6ec64e
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-12-04 11:18:39 +0200

    fix library modal

--- src/library.js
+++ src/library.js
@@ -1,7 +1,7 @@
 import './js/footer';
 import './js/header';
 import './js/header-library';
-// import './js/modal-film';
+import './js/modal-film';
 import './js/modal-team';
 import './js/my-library';
 import './js/darkmode';
