28d97e45cba1036e9127d438ce0b5cb609d2cc98
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-12-04 23:49:26 +0200

    corrected

--- src/js/header-library.js
+++ src/js/header-library.js
@@ -7,11 +7,11 @@ headerWatchedButtonEl.addEventListener('click', onWatched);
 headerQueueButtonEl.addEventListener('click', onQueue);
 
 function onWatched() {
-  headerWatchedButtonEl.classList.toggle('header__button--active');
+  headerWatchedButtonEl.classList.add('header__button--active');
   headerQueueButtonEl.classList.remove('header__button--active');
 }
 
 function onQueue() {
   headerWatchedButtonEl.classList.remove('header__button--active');
-  headerQueueButtonEl.classList.toggle('header__button--active');
+  headerQueueButtonEl.classList.add('header__button--active');
 }
