766c682bb0278a44d1d8de01c4114c99654ffb2b
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-12-04 22:37:57 +0200

    removed empty files and useless imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './js/films';
-// import './js/footer';
-// import './js/header';
 import './js/modal-film';
 import './js/modal-team';
 import './js/spinner';
--- src/library.js
+++ src/library.js
@@ -1,7 +1,4 @@
-// import './js/footer';
-// import './js/header';
 import './js/header-library';
-// import './js/modal-film';
 import './js/modal-film-library';
 import './js/modal-team';
 import './js/my-library';
