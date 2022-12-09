d074deba8bb4057aa5be87fabe7ba14585679058
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-12-09 23:27:54 +0200

    remove preloader

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,11 +1,3 @@
-window.onload = function () {
-  const preloader = document.querySelector('.spinner');
-  setTimeout(function () {
-    preloader.classList.add('visually-hidden');
-  }, 2000);
-};
-
-
 //===================== ORANGE SPINNER ASYNC =============//
 
 import { Spinner } from 'spin.js';
