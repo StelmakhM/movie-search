4e0dc83aa39da0174c8172ae879f1eb963a8bd76
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-02-18 19:39:55 +0200

    remove auth, refactor

--- src/library.js
+++ src/library.js
@@ -4,6 +4,3 @@ import './js/modal-team';
 import './js/my-library';
 import './js/darkmode';
 import './js/up-button';
-// === registration ====
-import './js/fireBaseApp/firebase-app';
-import './js/registration-form';
